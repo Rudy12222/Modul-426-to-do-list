f6f56f4ac80da993ad22aa5d8e106bbcf947cdf1
itsme <me.its18@yandex.ru>
2026-02-07 10:13:56 +0100

    modified:   Projekt_Code/list-delete.php

--- Projekt_Code/list-delete.php
+++ Projekt_Code/list-delete.php
@@ -1,7 +1,6 @@
 <?php include "view/header.php"; // Shared header markup ?>
 
-
-
+<button type="submit">Are you sure?</button>
 
 <script src="controller/list-delete-controller.js" defer></script>
 <?php include "view/footer.php"; // Shared footer markup ?>
